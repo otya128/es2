b4a43774202aeeb976ff5150c6b88e6d763b64a0
otya <otya281@gmail.com>
2025-09-04 21:20:00 +0900

    deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    base: process.env.NODE_ENV === "production" ? "/es2/" : "/",
+    build: {
+        outDir: "dist",
+    },
     test: {
         coverage: {
             reporter: ["text", "html"],
         },
-        include: ["src\/*.{test,spec}.?(c|m)[jt]s?(x)"],
+        include: ["src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
     },
 });
