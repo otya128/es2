5fe5355365b457ecb2ddcbb0e183b043f095d689
otya <otya281@gmail.com>
2025-09-23 13:16:33 +0900

    Fix position of callOperator

--- src/index.ts
+++ src/index.ts
@@ -2106,7 +2106,7 @@ function parseLeftHandSideExpression(tokenizer: Tokenizer): LeftHandSideExpressi
                 expression: left,
                 argumentList: parseArgumentList(tokenizer),
                 start: left.start,
-                end: tokenizer.prevPosition,
+                end: tokenizer.current.end,
             };
             const token = tokenizer.current;
             if (token.type !== "punctuator" || token.value !== ")") {
